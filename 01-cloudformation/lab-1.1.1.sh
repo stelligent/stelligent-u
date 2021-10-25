dcac4420bc9a198092af2d2123f5d1b63eb4708a
John Watson <jwatson3d@gmail.com>
2021-10-25 16:36:19 -0500

    lab 1.1.1 most minimal CFN template possible

---
+++ 01-cloudformation/lab-1.1.1.sh
@@ -0,0 +1,3 @@
+aws cloudformation create-stack --stack-name jwcfns3bucketstack --template-body file://cfn-template.yml
+
+# "StackId": "arn:aws:cloudformation:us-east-1:858633482938:stack/jwcfns3bucketstack/0fa90d90-35db-11ec-80c5-0a6aa40b27d1"
