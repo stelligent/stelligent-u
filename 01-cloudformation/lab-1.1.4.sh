6033b2b7736f8891d9c95751f8f522946a932be8
John Watson <jwatson3d@gmail.com>
2021-10-26 11:25:02 -0500

    lab 1.1.4 add conditional logic - preferred region

---
+++ 01-cloudformation/lab-1.1.4.sh
@@ -0,0 +1,3 @@
+ aws cloudformation create-stack --stack-name jw-m01x114-cfns3bucketstack --template-body file://cfn-template.yml --parameters file://parameters.json
+
+#  "StackId": "arn:aws:cloudformation:us-east-1:858633482938:stack/jw-m01x112-cfns3bucketstack/650f2630-35e4-11ec-bed8-12097f4b4465"
