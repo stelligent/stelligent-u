101d1dae09b7430aded0f24582e20bc619856bc5
Matthew Gable <matthew.gable@MACUSSTG2524595.local>
2022-06-16 14:54:55 -0400

    1.1.3

--- 01-cloudformation/stack.sh
+++ 01-cloudformation/stack.sh
@@ -1,3 +1,7 @@
 #/bin/bash
 
-aws cloudformation update-stack --stack-name mattgstack --template-body file://cfn-bucket.yaml --parameters file://cfn-bucket-params.json
+aws cloudformation create-stack --stack-name mattgstack --template-body file://cfn-bucket.yaml --parameters file://cfn-bucket-params.json
+
+#aws cloudformation delete-stack --stack-name mattgstack
+
+#aws cloudformation validate-template --template-body file://cfn-bucket.yaml
