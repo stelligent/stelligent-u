95fdbbd788375fb9995e08817de30cb173d68c2c
Matthew Gable <matthew.gable@MACUSSTG2524595.local>
2022-06-17 14:41:21 -0400

    2.1.1

---
+++ 02-s3/commands.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+# This is a document that shows work when there are not any mandatory file changes
+
+exit
+
+2.1.1
+aws s3api create-bucket --region us-west-2 --bucket stelligent-u-matthew.gable.labs --create-bucket-configuration LocationConstraint=us-west-2
+aws s3api list-objects --bucket stelligent-u-matthew.gable.labs
---
+++ 02-s3/stack.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+#aws cloudformation validate-template --template-body file://import-stack.yaml
+
+#aws cloudformation create-stack --stack-name mattgimportstack --capabilities CAPABILITY_NAMED_IAM --template-body file://import-stack.yaml
+
+#aws cloudformation delete-stack --stack-name mattgimportstack
