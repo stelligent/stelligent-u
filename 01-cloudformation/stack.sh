9da1d2a348fdb6acf880398aca1bfe7d44b71459
Matthew Gable <matthew.gable@MACUSSTG2524595.local>
2022-06-17 11:02:55 -0400

    lab 1.2.1

--- 01-cloudformation/stack.sh
+++ 01-cloudformation/stack.sh
@@ -1,7 +1,13 @@
 #/bin/bash
 
+# OLD
 #aws cloudformation create-stack --stack-name mattgstack --template-body file://cfn-bucket.yaml --parameters file://cfn-bucket-params.json
+################
+
+
+#aws cloudformation create-stack --stack-name mattgstack --capabilities CAPABILITY_NAMED_IAM --template-body file://cfn-user.yaml
 
 aws cloudformation delete-stack --stack-name mattgstack
 
-#aws cloudformation validate-template --template-body file://cfn-bucket.yaml
+#aws cloudformation validate-template --template-body file://cfn-user.yaml
+
