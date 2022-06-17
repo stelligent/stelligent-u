a5e18fbf2164d5668d81dbbe15e7e60a0a52e87b
Matthew Gable <matthew.gable@MACUSSTG2524595.local>
2022-06-17 12:30:24 -0400

    1.2.3

--- 01-cloudformation/stack.sh
+++ 01-cloudformation/stack.sh
@@ -6,9 +6,16 @@
 
 
 #aws cloudformation create-stack --stack-name mattgstack --capabilities CAPABILITY_NAMED_IAM --template-body file://cfn-user.yaml
-aws cloudformation update-stack --stack-name mattgstack --capabilities CAPABILITY_NAMED_IAM --template-body file://cfn-user.yaml
+#aws cloudformation update-stack --stack-name mattgstack --capabilities CAPABILITY_NAMED_IAM --template-body file://cfn-user.yaml
 
 #aws cloudformation delete-stack --stack-name mattgstack
 
 #aws cloudformation validate-template --template-body file://cfn-user.yaml
 
+######
+# import
+#aws cloudformation validate-template --template-body file://import-stack.yaml
+
+aws cloudformation create-stack --stack-name mattgimportstack --capabilities CAPABILITY_NAMED_IAM --template-body file://import-stack.yaml
+
+#aws cloudformation delete-stack --stack-name mattgimportstack
