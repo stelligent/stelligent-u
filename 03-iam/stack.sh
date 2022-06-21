c25edb401c24c1b21d9c476587d47c088c9cc15a
Matthew Gable <matthew.gable@MACUSSTG2524595.local>
2022-06-21 15:23:44 -0400

    3.1.4

--- 03-iam/stack.sh
+++ 03-iam/stack.sh
@@ -2,7 +2,7 @@
 
 #aws cloudformation validate-template --template-body file://iam.yaml
 
-#@aws cloudformation create-stack --stack-name mattgiamstack --capabilities CAPABILITY_NAMED_IAM --template-body file://iam.yaml
+#aws cloudformation create-stack --stack-name mattgiamstack --capabilities CAPABILITY_NAMED_IAM --template-body file://iam.yaml
 
 aws cloudformation update-stack --stack-name mattgiamstack --capabilities CAPABILITY_NAMED_IAM --template-body file://iam.yaml
 
